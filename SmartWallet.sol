ec4b264d18525ce0eed6776e4628cfe28bd7e02e
thiago <thiagosteps@gmail.com>
2022-08-12 10:23:32 +0100

    added 1 smart contract

---
+++ SmartWallet.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.5.13;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+contract SharedWallet is Ownable {
+
+    mapping (address => uint256) allowance;
+
+    function addAllowance(address _who, uint256 _amount) public onlyOwner {
+        allowance[_who] = _amount;
+    }
+
+    modifier ownerOrAllowed(uint256 _amount) {
+        require(isOwner() || allowance[msg.sender] >= _amount, "You are not allowed");
+        _;
+    }
+
+    function withdrawMoney(address payable _to, uint _amount) public payable ownerOrAllowed(_amount) {
+        
+        _to.transfer(_amount);
+    }
+
+    receive() external payable {
+
+    }
+}
+:wq
+
